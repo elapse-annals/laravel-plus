7a2c19c2e9ff08229f3a1e172be8dc167d300c19
ben <benhuang1024@gmail.com>
2019-05-11 11:11:12 +0800

    增加 controller 模版文件

---
+++ storage/app/framework_temp/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Presenters\TestPresenter;
+use App\Transformers\TestTransformer;
+use App\Formatters\TestFormatter;
+
+
+class TestController extends Controller
+{
+    //
+    public function handle()
+    {
+
+    }
+}
