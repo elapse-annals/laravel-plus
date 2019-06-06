6424e07b01e468fbfb0cd3107c44bd051b35bbd1
benhuang <benhuang1024@gmail.com>
2019-06-07 00:13:44 +0800

    调整模版布局

---
+++ storage/app/tmpl/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    @include('temp.head')
+</head>
+<body>
+<div class="container">
+    <header class="row">
+        @include('temp.header')
+    </header>
+    <div id="main" class="row">
+        @yield('content')
+    </div>
+    <footer class="row">
+        @include('temp.footer')
+    </footer>
+</div>
+</body>
+</html>
