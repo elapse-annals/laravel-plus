1f5bcc3122295cb25d04b003ff51f6d6299bd947
ben <benhuang1024@gmail.com>
2019-07-09 21:13:42 +0800

    增加 where string 版本

--- app/Services/WhereService.php
+++ app/Services/WhereService.php
@@ -87,4 +87,57 @@ class WhereService extends Service
         }
         return $limit;
     }
+
+    public static function joinSearchString(array $request_data, array $search_map, $where = "", array $search_accurate_arr = [])
+    {
+        if ($request_data['search']) {
+            foreach ($request_data['search'] as $key => $value) {
+                if (((is_string($value) && '' != $value) || (is_array($value) && !empty($value))) && $search_map[$key]) {
+                    switch ($key) {
+                        case 'created_at':
+                            $where .= self::getBetweenDateStr($value['start'], $value['end'], '', $search_map[$key]);
+                            break;
+                        case 'updated_at':
+                            $where .= self::getBetweenDateStr($value['start'], $value['end'], '', $search_map[$key]);
+                            break;
+                        case 'verification_at':
+                            $where .= self::getBetweenDateStr($value['start'], $value['end'], '', $search_map[$key]);
+                            break;
+                        default:
+                            if (is_array($value)) {
+                                $condition = "";
+                                foreach ($value as $handle) {
+                                    $condition .= "'{$handle}'" . ',';
+                                }
+                                $condition = trim($condition, ',');
+                                $where .= $search_map[$key] . ' IN (' . $condition . ') AND ';
+                            } else {
+                                if (($search_accurate_arr && in_array($key, $search_accurate_arr)) || isset($search_accurate_arr['all'])) {
+                                    $where .= $search_map[$key] . ' = \'' . $value . '\' AND ';
+                                }
+                            }
+                    }
+                }
+            }
+        }
+        $where = trim($where, 'AND ');
+        $limit = self::joinPage($request_data);
+        return [$where, $limit];
+    }
+
+    public static function getBetweenDateString($start_date, $end_date, $where, $date_key)
+    {
+        if (!empty($start_date) && !empty($end_date)) {
+            $start_date .= ' 00:00:00';
+            $end_date .= ' 23:59:59';
+            $where = $date_key . ' BETWEEN ' . "'$start_date'" . ' AND ' . "'$end_date'" . ' AND ';
+        } elseif (empty($start_date) && !empty($end_date)) {
+            $end_date .= ' 23:59:59';
+            $where = $date_key . ' <= ' . "'$end_date'" . ' AND ';
+        } elseif (empty($end_date) && !empty($start_date)) {
+            $start_date .= ' 00:00:00';
+            $where = $date_key . ' >= ' . "'$start_date'" . ' AND ';
+        }
+        return $where;
+    }
 }
