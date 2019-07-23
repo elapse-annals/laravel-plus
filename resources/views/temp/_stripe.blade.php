8d1bcfc96d8a68701ae281657b3b35bc40aa2537
ben <benhuang1024@gmail.com>
2019-07-23 15:07:48 +0800

    修复方法引入

--- resources/views/temp/_stripe.blade.php
+++ resources/views/temp/_stripe.blade.php
@@ -8,7 +8,7 @@
             </a>
         </el-col>
         <el-col :span="4">
-            <el-button type="primary" plain="true" @click="onSubmit">查询</el-button>
+            <el-button type="primary" plain="true" @click="onSubmit()">查询</el-button>
             <el-button plain="true" @click="resetForm('search')">重置</el-button>
             {{--            <el-button type="danger" plain="true" @click="deletes('ids')">删除</el-button>--}}
         </el-col>
