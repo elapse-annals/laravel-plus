f44bfdd1b56d5cc541b8842abf9181e8ca9dbe59
benhuang <benhuang1024@gmail.com>
2019-08-01 00:16:25 +0800

    优化操作按钮样式

--- resources/views/temp/_stripe.blade.php
+++ resources/views/temp/_stripe.blade.php
@@ -1,18 +1,18 @@
 <template>
     <el-row>
-        <el-col :span="20">
+        <el-col :span="19">
             <a href="/temps/create">
-                <el-button type="primary" plain="true">
+                <el-button type="primary" plain="true" icon="el-icon-plus">
                     新增
                 </el-button>
             </a>
             <a href="/export/temps" target="_blank">
-                <el-button type="primary" plain="true">导出<i class="el-icon-download el-icon--right"></i></el-button>
+                <el-button type="primary" plain="true" icon="el-icon-download">导出</el-button>
             </a>
         </el-col>
-        <el-col :span="4">
+        <el-col :span="5">
             <el-button type="primary" plain="true" @click="onSubmit()" icon="el-icon-search">查询</el-button>
-            <el-button plain="true" @click="resetForm('search')">重置</el-button>
+            <el-button plain="true" @click="resetForm('search')" icon="el-icon-refresh-left">重置</el-button>
             {{--            <el-button type="danger" plain="true" @click="deletes('ids')">删除</el-button>--}}
         </el-col>
     </el-row>
