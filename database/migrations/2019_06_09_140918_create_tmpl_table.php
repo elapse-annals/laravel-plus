71958371271ab38dd907639b8c47890980b31d45
benhuang <benhuang1024@gmail.com>
2019-08-02 23:43:11 +0800

    todo

--- database/migrations/2019_06_09_140918_create_tmpl_table.php
+++ database/migrations/2019_06_09_140918_create_tmpl_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTempTable extends Migration
+class CreateTmplTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTempTable extends Migration
      */
     public function up()
     {
-        Schema::create('temps', function (Blueprint $table) {
+        Schema::create('tmpls', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name')->unique()->comment('名称');
             $table->tinyInteger('sex')->comment('性别');
@@ -37,6 +37,6 @@ class CreateTempTable extends Migration
      */
     public function down()
     {
-        Schema::drop('temps');
+        Schema::drop('tmpls');
     }
 }
