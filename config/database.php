d16cfbd49617bf8e149e0ac7a0bc1d444bab8765
benhuang1024 <benhuang1024@gmail.com>
2019-12-09 20:43:46 +0800

    update predis to phpredis

--- config/database.php
+++ config/database.php
@@ -117,10 +117,10 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'predis'),
+        'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'predis'),
+            'cluster' => env('REDIS_CLUSTER', 'phpredis'),
             'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
         ],
 
