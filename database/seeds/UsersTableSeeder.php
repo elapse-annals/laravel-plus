43dfc466afb55f519c340de831047c32a0f12ab3
benhuang1024 <benhuang1024@gmail.com>
2020-02-02 16:01:08 +0800

    add users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(User::class, 100000)
+            ->create();
+    }
+}
