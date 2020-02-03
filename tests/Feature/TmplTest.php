c8c4def0e140073c0905d3db92007507aa68e504
benhuang1024 <benhuang1024@gmail.com>
2020-02-03 21:59:16 +0800

    feature test todo

--- tests/Feature/TmplTest.php
+++ tests/Feature/TmplTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Tmpl;
+use Faker\Generator as Faker;
 
 class TmplTest extends TestCase
 {
@@ -13,10 +15,38 @@ class TmplTest extends TestCase
      *
      * @return void
      */
-    public function testTmpls()
+    public function testGetTmpls()
     {
         $response = $this->get('/tmpls');
 
         $response->assertStatus(200);
+    }
+
+    public function testPostTmpls()
+    {
+        $faker = new Faker();
+        $data = [
+            'name' => $faker->name,
+            'sex'  => $faker->numberBetween(0, 1),
+        ];
+        $response = $this->post('/tmpls', $data);
+        $response->assertStatus(200);
+    }
+
+    public function testPutTmpls()
+    {
+        $tmpl = Tmpl::first();
+        $tmpl->name = 'test';
+        $response = $this->put('/tmpls/' . $tmpl->id, $tmpl);
+        $response->assertStatus(200);
+    }
+
+    public function testDeleteTmpls()
+    {
+        $tmpl = Tmpl::first();
+        $response = $this->delete('/tmpls/' . $tmpl->id);
+        echo $tmpl->id;
+        $response->assertStatus(200);
+        $this->isNull(Tmpl::find($tmpl->id));
     }
 }
