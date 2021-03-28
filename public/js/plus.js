8c3b82484e80737d605e4fc40b0385c4e77030df
benhuang <benhuang1024@gmail.com>
2021-03-28 16:02:43 +0800

    plus

---
+++ public/js/plus.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./resources/js/plus.js ***!
+  \******************************/
+setTimeout(function () {
+  var aside = document.getElementById('aside');
+
+  if (aside) {
+    aside.style.display = "block";
+  }
+}, 100);
+/******/ })()
+;
