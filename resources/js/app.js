b4423200cb92869b34e76f3ce8d47d87f9a55b17
benhuang <benhuang1024@gmail.com>
2021-03-28 09:50:43 +0800

    add mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
 require('./bootstrap');
 
-window.Vue = require('vue');
-
+import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
+
 const app = new Vue({
     el: '#app',
     mixins: [mixinSlot]
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,13 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
+})
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/jet-stream.js', 'public/js')
